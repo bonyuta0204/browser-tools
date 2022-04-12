cb9f91a8f50e55c8c1aaa499d62bd24c3e85ede1
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-04-12 10:55:23 +0900

    fix: freeeへのログイン処理を実装

--- browser_tools/dakoku.py
+++ browser_tools/dakoku.py
@@ -5,45 +5,56 @@ from os import getenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Chrome()
 
+class FreeeDakoku:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
-def site_login():
-    # 最大待ち時間
-    driver.get("https://stg.kpiee.xyz/users/sign_in")
-    wait_time = 10
-    wait = WebDriverWait(driver, wait_time)
+    def login(self):
+        # 最大待ち時間
+        driver.get("https://accounts.secure.freee.co.jp/login/hr")
+        wait_time = 10
+        wait = WebDriverWait(driver, wait_time)
 
-    wait.until(lambda d: d.find_element(by=By.CSS_SELECTOR,
-                                        value='input[type="email"]'))
+        wait.until(lambda d: d.find_element(by=By.CSS_SELECTOR,
+                                            value='input[type="email"]'))
 
-    email_el = driver.find_element(by=By.CSS_SELECTOR,
-                                   value='input[type="email"]')
-    password_el = driver.find_element(by=By.CSS_SELECTOR,
-                                      value='input[type="password"]')
-    checkbox_el = driver.find_element(by=By.CSS_SELECTOR,
-                                      value="input.bl_kpCheckbox+label")
-    submit_button_el = driver.find_element(by=By.CSS_SELECTOR,
-                                           value=".bl_kpBtn.bl_kpBtn__login")
+        email_el = driver.find_element(by=By.CSS_SELECTOR,
+                                       value='input[type="email"]')
+        password_el = driver.find_element(by=By.CSS_SELECTOR,
+                                          value='input[type="password"]')
+        submit_el = driver.find_element(by=By.CSS_SELECTOR,
+                                          value='input[type="submit"]')
 
-    email = getenv('KPIEE_EMAIL')
-    password = getenv('KPIEE_PASSWORD')
+        email = getenv('FREEE_EMAIL')
+        password = getenv('FREEE_PASSWORD')
 
-    time.sleep(2)
+        time.sleep(2)
 
-    checkbox_el.click()
-    email_el.send_keys(email)
-    password_el.send_keys(password)
-    submit_button_el.click()
+        email_el.send_keys(email)
+        password_el.send_keys(password)
+        submit_el.click()
+
+        try: 
+            wait.until(lambda d: 'https://p.secure.freee.co.jp' in d.current_url)
+        except TimeoutException: 
+            raise Exception('ログインに失敗しました')
+
+
+        print('login succeeded')
+
+
+
+    def quit(self):
+        self.driver.quit()
 
 
 if __name__ == '__main__':
+    dakoku = FreeeDakoku()
+    dakoku.login()
+    time.sleep(10)
+    dakoku.quit()
 
-    driver.get("https://stg.kpiee.xyz/users/sign_in")
-
-    print(driver.title)
-
-    site_login()
-
-    driver.quit()
--- browser_tools/driver_manager.py
+++ browser_tools/driver_manager.py
@@ -5,3 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def install_chrome_driver():
     webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+if __name__ == '__main__':
+    install_chrome_driver()
